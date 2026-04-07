f452b3200eb900634b0fa3494c33f3770a22ae1d
nunoluciano <1905497+gigamaster@users.noreply.github.com>
2026-04-07 14:04:03 +0200

    notes types

---
+++ src/components/notes/types.ts
@@ -0,0 +1,28 @@
+import { Tag } from "@/types";
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  site_id?: string;
+  tags: string[];
+  is_encrypted: boolean;
+  content_iv?: string;
+  content_ciphertext?: string;
+  content_salt?: string;
+  created_at: number;
+  updated_at: number;
+}
+
+export interface Site {
+  id: string;
+  domain: string;
+}
+
+export interface NoteFormData {
+  title: string;
+  content: string;
+  site_id: string;
+  tags: string[];
+  is_encrypted: boolean;
+}
